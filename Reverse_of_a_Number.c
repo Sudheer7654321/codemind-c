7325c27416af94c46613f1828b3ceaceb2920213
Sudheer7654321 <147792492+Sudheer7654321@users.noreply.github.com>
2023-10-23 12:16:31 +0530

    #include <stdio.h>
    int main() {
    
      int n, reverse = 0, r;
    
      scanf("%d", &n);
    
      while (n != 0) {
        r = n % 10;
        reverse = reverse * 10 + r;
        n /= 10;
      }
    
      printf("%d",reverse);
    
      return 0;
    }

---
+++ Reverse_of_a_Number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+  int n, reverse = 0, r;
+
+  scanf("%d", &n);
+
+  while (n != 0) {
+    r = n % 10;
+    reverse = reverse * 10 + r;
+    n /= 10;
+  }
+
+  printf("%d",reverse);
+
+  return 0;
+}
