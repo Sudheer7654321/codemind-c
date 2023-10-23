acf12063726d660ed99c90ae3e50f7654c1fcaea
Sudheer7654321 <147792492+Sudheer7654321@users.noreply.github.com>
2023-10-23 12:56:32 +0530

    #include <stdio.h>
    int main() {
    
      int m,n,p;
    
      scanf("%d%d",&m,&n);
      int c=0;
      
      for(int i=m;i<=n;i++) {
          if(i%3==0){
              c++;
          }
          
       
      }
    
      printf("%d ",c);
    
      return 0;
    }

---
+++ Count_Multiples_of_3_between_M_,N.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+  int m,n,p;
+
+  scanf("%d%d",&m,&n);
+  int c=0;
+  
+  for(int i=m;i<=n;i++) {
+      if(i%3==0){
+          c++;
+      }
+      
+   
+  }
+
+  printf("%d ",c);
+
+  return 0;
+}
