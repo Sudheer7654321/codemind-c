f8ac138df8079057e92c4bc11a2bfd4b71e1b4bf
Sudheer7654321 <147792492+Sudheer7654321@users.noreply.github.com>
2023-11-20 19:47:13 +0530

    #include<stdio.h> int main(){     int a,b,c,d,i;     scanf("%d",&a);     if(a%3==0) printf("Pling");      if (a%5==0) printf("Plang");      if (a%7==0) printf("Plong");     if(a%3!=0 && a%5!=0 && a%7!=0) printf("%d",a); }

---
+++ RainDrop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,i;
+    scanf("%d",&a);
+    if(a%3==0) printf("Pling");
+     if (a%5==0) printf("Plang");
+     if (a%7==0) printf("Plong");
+    if(a%3!=0 && a%5!=0 && a%7!=0) printf("%d",a);
+}
