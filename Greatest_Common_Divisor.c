ac5ee27c50b99831b617795fe9b287599a7fec76
Sudheer7654321 <147792492+Sudheer7654321@users.noreply.github.com>
2023-11-21 12:30:32 +0530

    #include <stdio.h> int main() {     int n,m,i,lar=0,a;     scanf("%d%d",&n,&m);     for(i=1;i<=m || i<=n ;i++){     	a=(n%i==0 && m%i==0) ? i : 0;     	 if(a!=0){     	 	lar=(lar<a) ? a :0; 		 } 		} 		if(lar!=0){ 		printf("%d",lar); 	} 	}

---
+++ Greatest_Common_Divisor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n,m,i,lar=0,a;
+    scanf("%d%d",&n,&m);
+    for(i=1;i<=m || i<=n ;i++){
+    	a=(n%i==0 && m%i==0) ? i : 0;
+    	 if(a!=0){
+    	 	lar=(lar<a) ? a :0; 
+		 }
+		}
+		if(lar!=0){
+		printf("%d",lar);
+	}
+	}
+
+    
