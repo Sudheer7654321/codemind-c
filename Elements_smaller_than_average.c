3b4492d2bdc64fbc09297492bbaf7dc2691677a5
Sudheer7654321 <147792492+Sudheer7654321@users.noreply.github.com>
2024-03-18 14:20:29 +0530

    #include <stdio.h> void scan(int n, int *a) {     for (int i = 0; i < n; i++) {         scanf("%d", &a[i]);     } }
    int main() {
        int n, f = 0,c=0;
        scanf("%d", &n);
        int a[n];
        scan(n, a);
        for (int i = 0; i < n; i++) {
           f=f+a[i];
        }
        f=f/n;
        for (int i = 0; i < n; i++) {
           if(f>=a[i]){
               c++;
           }
        }
        printf("%d",c);
    }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void scan(int n, int *a) {
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+}
+
+int main() {
+    int n, f = 0,c=0;
+    scanf("%d", &n);
+    int a[n];
+    scan(n, a);
+    for (int i = 0; i < n; i++) {
+       f=f+a[i];
+    }
+    f=f/n;
+    for (int i = 0; i < n; i++) {
+       if(f>=a[i]){
+           c++;
+       }
+    }
+    printf("%d",c);
+}
