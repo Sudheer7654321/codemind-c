f5ef889456d38f95e45babbd63197764d2695c0c
Sudheer7654321 <147792492+Sudheer7654321@users.noreply.github.com>
2024-03-18 10:08:55 +0530

    #include<stdio.h> int main(){     int i, j, n, k;     scanf("%d", &n);     for(i = 1; i <= n; i++){         if(i % 2 == 0){             for(j = 1; j <= n; j++){                 printf("0 ");             }             printf(" ");         }         else{             for(k = 1; k <= n; k++){                 printf("1 ");             }             printf(" ");         }     }     return 0; }

---
+++ Basic_Pattern_4.2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int i, j, n, k;
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+        if(i % 2 == 0){
+            for(j = 1; j <= n; j++){
+                printf("0 ");
+            }
+            printf("
+");
+        }
+        else{
+            for(k = 1; k <= n; k++){
+                printf("1 ");
+            }
+            printf("
+");
+        }
+    }
+    return 0;
+}
