b0b640dc42f3e401b912b8d7fa3de902026db303
Sudheer7654321 <147792492+Sudheer7654321@users.noreply.github.com>
2024-03-18 10:24:02 +0530

    #include<stdio.h> int main(){     int i, j, n, k,m=1;     scanf("%d", &n);     for(i = n; i >= 1; i--){
                for(j = 1; j <=i ; j++){
                    printf("%d",i);
                }
                for(k = n-i; k > 0 ; k--){
                    printf("*");
                }
                printf("
    ");
                m++;
        }
        return 0;
    }

---
+++ Basic_Pattern_5.3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int i, j, n, k,m=1;
+    scanf("%d", &n);
+    for(i = n; i >= 1; i--){
+        
+            for(j = 1; j <=i ; j++){
+                printf("%d",i);
+            }
+            for(k = n-i; k > 0 ; k--){
+                printf("*");
+            }
+            printf("
+");
+            m++;
+    }
+    return 0;
+}
