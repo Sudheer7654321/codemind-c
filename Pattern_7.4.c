8173b7b74ed75c0cf449035e7dc8fdbb38461f99
Sudheer7654321 <147792492+Sudheer7654321@users.noreply.github.com>
2024-03-18 12:08:19 +0530

    #include<stdio.h> int main() {     int a,b=1,i,j,k,x;     scanf("%d",&a); x=a;     for(i=a;i>=1;i--)     {         x=a;         x=x-i;         for(j=a-i;j>0;j--)         {             printf(" ");         }         for(k=i;k>=1;k--)         {             printf("%c ",64+i);             x++;
            }
    
            printf("
    ");
            b++;
        }
    }

---
+++ Pattern_7.4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a,b=1,i,j,k,x;
+    scanf("%d",&a);
+x=a;
+    for(i=a;i>=1;i--)
+    {
+        x=a;
+        x=x-i;
+        for(j=a-i;j>0;j--)
+        {
+            printf(" ");
+        }
+        for(k=i;k>=1;k--)
+        {
+            printf("%c ",64+i);
+            x++;
+            
+        }
+
+        printf("
+");
+        b++;
+    }
+}
