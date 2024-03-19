54c7d8f97ed4d3ba19b17dada0b4ff4f678a3f92
SUDHEER KONDAMURI <147792492+Sudheer7654321@users.noreply.github.com>
2024-03-19 14:13:42 +0530

    #include<stdio.h> int main(){     int n,i,j;     scanf("%d",&n);     for(i=n;i>0;i--){         for(j=i;j>0;j--){             printf("%c ",64+i);         }         printf(" ");     } }

--- Basic_Pattern_3.4.c
+++ Basic_Pattern_3.4.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-    int i,j,n;
+    int n,i,j;
     scanf("%d",&n);
-    for(i=n;i>=1;i--){
-        for(j=i;j>=1;j--){
-            printf("%c ",i+64);
+    for(i=n;i>0;i--){
+        for(j=i;j>0;j--){
+            printf("%c ",64+i);
         }
         printf("
 ");
